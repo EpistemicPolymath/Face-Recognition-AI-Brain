ffe0750488f1235dd21207786e88e4e31da614ea
polymathical <epistemicpolymath@gmail.com>
2019-06-13 14:58:06 -0400

    Setup work environment for the Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+// Components
+import Navigation from './components/Navigation/Navigation.js';
 
 function App() {
   return (
     <div className="App">
         <Navigation />
-        <Logo />
-        <ImageLinkForm />
-        <FaceRecognition />
+      {/*<Logo />
+      <ImageLinkForm />
+      <FaceRecognition />*/}
     </div>
   );
 }
