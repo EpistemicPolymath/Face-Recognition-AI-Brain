90a76154691061fecd49b28239cae61b9ef629da
polymathical <epistemicpolymath@gmail.com>
2019-06-14 12:40:59 -0400

    Finished rank and ImageLinkForm component skeletons as well as installed react particles

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 // Components
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+
 
 function App() {
   return (
     <div className="App">
         <Navigation />
         <Logo />
-      {/* <ImageLinkForm />
+        <Rank />
+       <ImageLinkForm />
+       {/*
       <FaceRecognition />*/}
     </div>
   );
