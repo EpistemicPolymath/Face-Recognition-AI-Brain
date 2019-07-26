1bf27978b65bf67fef47126c7dc8599c1657ac45
polymathical <epistemicpolymath@gmail.com>
2019-07-26 13:01:55 -0400

    Added functionality to allow a user's rank to update upon successfully submitting new images for face detection suing image endpoint on the server.

--- src/App.js
+++ src/App.js
@@ -99,13 +99,30 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  onButtonSubmit = (event) => {
+  onImageSubmit = (event) => {
     this.setState({imgUrl: this.state.input});
     app.models.predict(
         Clarifai.FACE_DETECT_MODEL,
         // URL
         this.state.input)
-  .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+  .then(response => {
+    // As long as we have a response from the API
+    if (response) {
+      // Increment Image Entries at Endpoint
+      fetch('http://localhost:3000/image', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            id: this.state.user.id
+        })
+      })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}));
+        })
+    }
+  this.displayFaceBox(this.calculateFaceLocation(response))
+  })
   .catch(error => console.log(error));
   }
 
@@ -135,7 +152,7 @@ class App extends Component {
               <Logo />
               <Rank name={this.state.user.name} entries={this.state.user.entries}/>
              <ImageLinkForm
-               onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}
+               onInputChange={this.onInputChange} onImageSubmit={this.onImageSubmit}
              />
              <FaceRecognition
                box={box}
